886cd2ce8a4d7ae5e34490228b4b7dd21c508f59
= <=>
2023-12-11 16:54:40 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMode(){
+  const html = document.documentElement
+
+  // if(html.classList.contains('light')) {
+  // html.classList.remove('light')
+  //}
+  // else {
+  // html.classList.add('light')
+  // }
+  
+  
+  html.classList.toggle("light")
+
+// pegar o tag img
+
+// se tiver light mode, adicionar
+// substituir a imagem
+// se tiver light mode, adicionar a imagem light
+// set tiver sem light mode, manter a imagem normal
+
+}
